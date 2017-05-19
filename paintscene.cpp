7180d5046fb062111443d55c122736f18eb6b1d7
Unknown <johnbuevich@yahoo.com>
2017-05-19 18:42:54 +0300

    Принятие изменений с сервера в новом потоке и отрисовка по qtimer

--- paintscene.cpp
+++ paintscene.cpp
@@ -1,65 +1,113 @@
 #include "paintscene.h"
 #include <iostream>
 #include <vector>
+#include <chrono>
+#include <thread>
+#include <mutex>
+
+std::mutex my_mutex;
+
+void updatePoint( std::vector<points>& Vector_point ) // std::vector<double> NewPoints )
+{
+    while (true) {
+        std::cerr <<  "ПРоверка" << "\n";
+        bool status = true;
+        //status = IsStatus();
+        if (status == true) {
+//            std::vector<points> points (500);
+//            for (int i = 0; i < 500; i++) {
+//                points[i].x = i+ 5;
+//                points[i].y = i+ 15;
+//            }
+
+            std::vector<points> points;
+            //points = changes();
+            // примет изменения
+            // Паттерн команда
+            // результат перекинет в поток отрисовки и встанет в очередь на отрисовку или отрисуется сразу
+            my_mutex.lock();
+            Vector_point = points;
+//            v1.clear();
+//            v1.insert(v1.begin(), v2.begin(), v2.end());
+            my_mutex.unlock();
+        }
+
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+
+}
+
+
+void paintScene::updatePoints ()
+{
+    my_mutex.lock();
+    if (!Vector_point.empty()) {
+        for (int i = 0; i < Vector_point.size(); i++) {
+            addEllipse(Vector_point[i].x - 5,
+                       Vector_point[i].y - 5,
+                       10,
+                       10,
+                       QPen(Qt::NoPen),
+                       QBrush(Qt::red));
+        }
+        Vector_point.clear();
+    }
+    my_mutex.unlock();
+
+}
 
 paintScene::paintScene(QObject *parent) : QGraphicsScene(parent)
 {
     bool status = false;
     //status = IsStatus();
     if (status == true) {
-        std::vector<points> points;
         //points = changes();
-        for (int i = 0; i < points.size(); i++) {
-            addEllipse(points[i].x - 5,
-                       points[i].y - 5,
-                       10,
-                       10,
-                       QPen(Qt::NoPen),
-                       QBrush(Qt::red));
-        }
+//        my_mutex.lock();
+//        for (int i = 0; i < points.size(); i++) {
+//            addEllipse(points[i].x - 5,
+//                       points[i].y - 5,
+//                       10,
+//                       10,
+//                       QPen(Qt::NoPen),
+//                       QBrush(Qt::red));
+//        }
+//        my_mutex.unlock();
     }
 
+    // открываю поток по принятию изменений в вечном цикле вызывю updatePoint ///// QTread или qsocketnotifaer
+
+    std::thread thr (updatePoint, std::ref(Vector_point));
+
+    thr.detach();
+
     tmr = new QTimer(this); // Создаем объект класса QTimer и передаем адрес переменной
-    tmr->setInterval(1000); // Задаем интервал таймера
-    connect(tmr, &QTimer::timeout, this, &paintScene::updatePoint); // Подключаем сигнал таймера к нашему слоту
+    tmr->setInterval(110); // Задаем интервал таймера
+    connect(tmr, &QTimer::timeout, this, &paintScene::updatePoints); // Подключаем сигнал таймера к нашему слоту
     tmr->start(); // Запускаем таймер
 
 }
+
+
 
 paintScene::~paintScene()
 {
     tmr->stop();
 }
 
-void paintScene::updatePoint()
-{
-    std::cerr <<  "ПРоверка" << "\n";
 
-    bool status = false;
-    //status = IsStatus();
-    if (status == true) {
-        std::vector<points> points;
-        //points = changes();
-        for (int i = 0; i < points.size(); i++) {
-            addEllipse(points[i].x - 5,
-                       points[i].y - 5,
-                       10,
-                       10,
-                       QPen(Qt::NoPen),
-                       QBrush(Qt::red));
-        }
-    }
-}
 
 void paintScene::mousePressEvent(QGraphicsSceneMouseEvent *event)
 {
     // При нажатии кнопки мыши отрисовываем эллипс
+//    my_mutex.lock();
     addEllipse(event->scenePos().x() - 5,
                event->scenePos().y() - 5,
                10,
                10,
                QPen(Qt::NoPen),
                QBrush(Qt::red));
+//    my_mutex.unlock();
 
     points point;
     point.x = event->scenePos().x();
@@ -75,12 +123,14 @@ void paintScene::mousePressEvent(QGraphicsSceneMouseEvent *event)
 void paintScene::mouseMoveEvent(QGraphicsSceneMouseEvent *event)
 {
     // При нажатии кнопки мыши отрисовываем эллипс
+//    my_mutex.lock();
     addEllipse(event->scenePos().x() - 5,
                    event->scenePos().y() - 5,
                    10,
                    10,
                    QPen(Qt::NoPen),
                    QBrush(Qt::red));
+//    my_mutex.unlock();
 
     points point;
     point.x = event->scenePos().x();
--- paintscene.h
+++ paintscene.h
@@ -5,6 +5,15 @@
 #include <QGraphicsSceneMouseEvent>
 #include <QTimer>
 #include <QDebug>
+#include <vector>
+
+
+struct points {
+  int x, y;
+  int color;
+};
+
+
 
 class paintScene : public QGraphicsScene
 {
@@ -14,23 +23,20 @@ class paintScene : public QGraphicsScene
 public:
     explicit paintScene(QObject *parent = 0);
     ~paintScene();
+    std::vector<points> Vector_point;
 
 private:
     QPointF     previousPoint;      // Координаты предыдущей точки
     QTimer *tmr;
 
+
 private:
     // Для рисования используем события мыши
     void mousePressEvent(QGraphicsSceneMouseEvent * event);
     void mouseMoveEvent(QGraphicsSceneMouseEvent *event);
-    void updatePoint();
+    void updatePoints();
 };
 
 
-
-struct points {
-  int x, y;
-  int color;
-};
 
 #endif // PAINTSCENE_H
